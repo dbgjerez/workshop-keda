2fc0d7132bfb186881901b0e542ceae2c9f69205
David Borrego <dborrego@redhat.com>
2023-05-04 19:33:59 +0200

    jackon for stock

--- apps/book-crud/src/main/java/io/dborrego/domain/dto/Stock.java
+++ apps/book-crud/src/main/java/io/dborrego/domain/dto/Stock.java
@@ -1,15 +1,19 @@
 package io.dborrego.domain.dto;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import lombok.Data;
 
 @Data
-@Builder
 public class Stock {
 
     private String type;
     private Long id;
     private String title;
     private Integer quantity;
+
+    @JsonCreator
+    public Stock() {
+    }
     
 }
