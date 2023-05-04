58c98a718dc1c5160a1d2fd3fc42d0dc68d2d4f0
David Borrego <dborrego@redhat.com>
2023-05-04 15:21:47 +0200

    deployment

--- apps/stock-api/workaround.sh
+++ apps/stock-api/workaround.sh
@@ -5,7 +5,7 @@ openssl pkcs12 -in ca.p12 -password pass:$(oc get secret -n kafka kafka-cluster-
 openssl pkcs12 -in user.p12 -password pass:$(oc get secret -n kafka kafka-producer-stock-topic -o json | jq -r '.data["user.password"]' | base64 -d) -nokeys -out client.cer.pem
 openssl pkcs12 -in user.p12 -password pass:$(oc get secret -n kafka kafka-producer-stock-topic -o json | jq -r '.data["user.password"]' | base64 -d) -nodes -nocerts -out client.key.pem
 
-oc create secret generic kafka-connection \
+oc create secret generic -n dev-api kafka-connection \
  --from-file=server.cer.pem=server.cer.pem \
  --from-file=client.cer.pem=client.cer.pem \
  --from-file=client.key.pem=client.key.pem
