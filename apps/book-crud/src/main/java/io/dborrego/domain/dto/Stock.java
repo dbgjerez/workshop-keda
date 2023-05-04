6cac1ced13c8212f32ed97ded606f58e0ca971ee
David Borrego <dborrego@redhat.com>
2023-05-04 19:42:23 +0200

    0.1.3

--- apps/book-crud/src/main/java/io/dborrego/domain/dto/Stock.java
+++ apps/book-crud/src/main/java/io/dborrego/domain/dto/Stock.java
@@ -2,9 +2,6 @@ package io.dborrego.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 
-import lombok.Data;
-
-@Data
 public class Stock {
 
     private String type;
@@ -14,6 +11,38 @@ public class Stock {
 
     @JsonCreator
     public Stock() {
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
     }
     
 }
