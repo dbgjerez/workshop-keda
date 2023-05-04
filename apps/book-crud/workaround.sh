21fa5f0162d0d911ffca8bc3126c91d83e9c523c
David Borrego <dborrego@redhat.com>
2023-05-04 18:25:08 +0200

    debug

---
+++ apps/book-crud/workaround.sh
@@ -0,0 +1,8 @@
+oc get secret -n kafka kafka-cluster-ca-cert -o json | jq -r '.data["ca.p12"]' | base64 -d > ca.p12
+oc get secret -n kafka kafka-consumer-book-crud -o json | jq -r '.data["user.p12"]' | base64 -d > user.p12
+
+oc create secret generic -n dev-backend kafka-connection \
+ --from-file=ca.p12=ca.p12 \
+ --from-file=user.p12=user.p12 \
+ --from-literal=ca.password=$(oc get secret -n kafka kafka-cluster-ca-cert -o json | jq -r '.data["ca.password"]' | base64 -d) \
+ --from-literal=user.password=$(oc get secret -n kafka kafka-producer-stock-topic -o json | jq -r '.data["user.password"]' | base64 -d)
