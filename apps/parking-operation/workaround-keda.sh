16e8a5900b22a644e03d37f6d529093cd5296d15
David Borrego <dborrego@redhat.com>
2023-07-05 19:28:30 +0200

    create the own user for keda

--- apps/parking-operation/workaround-keda.sh
+++ apps/parking-operation/workaround-keda.sh
@@ -1,27 +1,14 @@
 oc get secret -n kafka kafka-cluster-ca-cert -o json | jq -r '.data["ca.p12"]' | base64 -d > ca.p12
-oc get secret -n kafka-consumer-parking-operation -o json | jq -r '.data["user.p12"]' | base64 -d > user.p12
+oc get secret -n kafka kafka-consumer-keda -o json | jq -r '.data["user.p12"]' | base64 -d > user.p12
 
-openssl pkcs12 \
-    -in ca.p12 \
-    -password pass:$(oc get secret -n kafka kafka-cluster-ca-cert -o json | jq -r '.data["ca.password"]' | base64 -d) \
-    -nokeys  \
-    -out server.cer.pem
-openssl pkcs12 \
-    -in user.p12 \
-    -password pass:$(oc get secret -n kafka-consumer-parking-operation -o json | jq -r '.data["user.password"]' | base64 -d) \
-    -nokeys \
-    -out client.cer.pem
-openssl pkcs12 \
-    -in user.p12 \
-    -password pass:$(oc get secret -n kafka-consumer-parking-operation -o json | jq -r '.data["user.password"]' | base64 -d) \
-    -nodes \
-    -nocerts \
-    -out client.key.pem
+openssl pkcs12 -in ca.p12 -password pass:$(oc get secret -n kafka kafka-cluster-ca-cert -o json | jq -r '.data["ca.password"]' | base64 -d) -nokeys -out server.cer.pem
+openssl pkcs12 -in user.p12 -password pass:$(oc get secret -n kafka kafka-consumer-keda -o json | jq -r '.data["user.password"]' | base64 -d) -nokeys -out client.cer.pem
+openssl pkcs12 -in user.p12 -password pass:$(oc get secret -n kafka kafka-consumer-keda -o json | jq -r '.data["user.password"]' | base64 -d) -nodes -nocerts -out client.key.pem
 
 oc create secret generic -n dev-backend keda-kafka-connection \
-    --from-file=server.cer.pem=server.cer.pem \
-    --from-file=client.cer.pem=client.cer.pem \
-    --from-file=client.key.pem=client.key.pem
+ --from-file=server.cer.pem=server.cer.pem \
+ --from-file=client.cer.pem=client.cer.pem \
+ --from-file=client.key.pem=client.key.pem
 
 rm -f server.cer.pem
 rm -f client.cer.pem
