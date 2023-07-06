ff2968774c883b35aa3e87b30cd52b04dbeb44d0
David Borrego <dborrego@redhat.com>
2023-07-06 18:24:42 +0200

    scalendobject

--- apps/camera-api/workaround.sh
+++ apps/camera-api/workaround.sh
@@ -1,7 +1,11 @@
 oc get secret -n kafka kafka-cluster-ca-cert -o json | jq -r '.data["ca.p12"]' | base64 -d > ca.p12
 oc get secret -n kafka kafka-producer-camera-api -o json | jq -r '.data["user.p12"]' | base64 -d > user.p12
 
-openssl pkcs12 -in ca.p12 -password pass:$(oc get secret -n kafka kafka-cluster-ca-cert -o json | jq -r '.data["ca.password"]' | base64 -d) -nokeys -out server.cer.pem
+openssl pkcs12 \
+    -in ca.p12 \
+    -password pass:$(oc get secret -n kafka kafka-cluster-ca-cert -o json | jq -r '.data["ca.password"]' | base64 -d) \
+    -nokeys \
+    -out server.cer.pem
 openssl pkcs12 -in user.p12 -password pass:$(oc get secret -n kafka kafka-producer-camera-api -o json | jq -r '.data["user.password"]' | base64 -d) -nokeys -out client.cer.pem
 openssl pkcs12 -in user.p12 -password pass:$(oc get secret -n kafka kafka-producer-camera-api -o json | jq -r '.data["user.password"]' | base64 -d) -nodes -nocerts -out client.key.pem
 
